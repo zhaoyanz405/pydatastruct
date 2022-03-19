e853087df7f5b0b7b798459200ef2466479a5974
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-03-19 23:51:50 +0800

    feat: add NodeQueue

--- collection/queue.py
+++ collection/queue.py
@@ -1,6 +1,7 @@
 """
 FIFO
 """
+from collection.node import Node, iter_nodes
 
 
 class EmptyQueueError(Exception):
@@ -29,3 +30,44 @@ class Queue:
         v = self.data[0]
         del self.data[0]
         return v
+
+
+class NodeQueue:
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.size = 0
+
+    def is_empty(self):
+        return self.first is None
+
+    def enqueue(self, item):
+        node = Node()
+        node.item = item
+
+        if self.is_empty():
+            self.first = node
+            self.last = self.first
+        else:
+            self.last.next = node
+            self.last = self.last.next
+
+        self.size += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            self.last = None
+            raise EmptyQueueError
+
+        item = self.first.item
+        self.first = self.first.next
+
+        self.size -= 1
+        return item
+
+    def __iter__(self):
+        return iter_nodes(self.first)
+
+
+Queue = NodeQueue
--- tests/collection/test_queue.py
+++ tests/collection/test_queue.py
@@ -3,12 +3,12 @@ import pytest
 from collection.queue import Queue, EmptyQueueError
 
 
-def test_queue_put_and_get():
+def test_queue():
     q = Queue()
 
     test_value = 'test'
-    q.put(test_value)
-    actual_value = q.get()
+    q.enqueue(test_value)
+    actual_value = q.dequeue()
     assert test_value == actual_value
 
 
@@ -16,12 +16,23 @@ def test_queue_is_empty():
     q = Queue()
     assert q.is_empty()
 
-    q.put(None)
+    q.enqueue(None)
     assert not q.is_empty()
 
 
-def test_queue_get_from_empty():
+def test_queue_enqueue_from_empty():
     q = Queue()
 
     with pytest.raises(EmptyQueueError):
-        q.get()
+        q.dequeue()
+
+
+def test_iter_queue():
+    q = Queue()
+
+    test_datas = [1, 2, 3, 4, 5]
+    for x in test_datas:
+        q.enqueue(x)
+
+    for index, item in enumerate(q):
+        assert item == test_datas[index]
